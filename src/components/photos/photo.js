a2a5a3019992e984a86bb57daf8ab7b8d7181e8d
MUNEER AHAMED SHAIK <61233544+Shaik648@users.noreply.github.com>
2021-01-28 01:17:09 +0530

    Updated Except Cancel Button

--- src/components/photos/photo.js
+++ src/components/photos/photo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Observer from "react-intersection-observer";
-
+const imageDownLoad = [];
 export default class Photo extends React.Component {
   constructor(props) {
     super(props);
@@ -9,23 +9,43 @@ export default class Photo extends React.Component {
       // imageDownLoad: [],
       imageGallary: this.props.photos,
       visibility: false,
-      indexValue: 0
+      indexValue: 0,
+      isChecked: false
     };
-    // this.imageChange = this.imageChange.bind(this);
+    this.imageChange = this.imageChange.bind(this);
+    this.fileDownloadHandler = this.fileDownloadHandler.bind(this);
     console.log("dataMatter", this.props);
   }
 
   imageChange = (pic) => {
-    var imageDownLoad = [];
+    console.log("pic", pic);
+
     if (imageDownLoad.includes(pic)) {
+      this.setState({
+        isChecked: !this.state.isChecked
+      });
       let x = imageDownLoad.indexOf(pic);
-      alert(x);
-      // imageDownLoad.splice(x, 1);
+      //  alert(x)
+      imageDownLoad.splice(x, 1);
     } else {
       imageDownLoad.push(pic);
     }
 
     console.log("dataMatter---", imageDownLoad);
+  };
+
+  fileDownloadHandler = async (pictures) => {
+    console.log("picts", pictures);
+    for (var i = 0; i < pictures.length; i++) {
+      const response = await fetch(pictures[i]);
+      response.blob().then((blob) => {
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.href = url;
+        a.download = "picture.jpeg";
+        a.click();
+      });
+    }
   };
 
   render() {
@@ -40,11 +60,23 @@ export default class Photo extends React.Component {
                 <img key={items.id} src={items.url} />
                 <input
                   type="checkbox"
+                  checked={this.state.isChecked}
                   onChange={() => this.imageChange(items.url)}
                 />
               </div>
             ))
           : ""}
+        <div>
+          <button onClick={() => this.fileDownloadHandler(imageDownLoad)}>
+            DownLoad{" "}
+          </button>
+          <button
+          // // onClick={() => this.fileDownloadHandler(imageDownLoad)}
+          
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     );
   }
