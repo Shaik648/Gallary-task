d3fa4c4f491d5bb1edc8c95de5daef573a05756f
MUNEER AHAMED SHAIK <61233544+Shaik648@users.noreply.github.com>
2021-01-28 02:27:50 +0530

    All Functionality Only Check Box State Update is Pending

--- src/components/photos/photo.js
+++ src/components/photos/photo.js
@@ -10,29 +10,56 @@ export default class Photo extends React.Component {
       imageGallary: this.props.photos,
       visibility: false,
       indexValue: 0,
-      isChecked: false
+      arrayLength: imageDownLoad.length,
+      // isChecked: false,
+      keyGen: []
     };
     this.imageChange = this.imageChange.bind(this);
     this.fileDownloadHandler = this.fileDownloadHandler.bind(this);
+    this.cancel = this.cancel.bind(this);
     console.log("dataMatter", this.props);
   }
 
-  imageChange = (pic) => {
-    console.log("pic", pic);
+  //   updateStateList(e, value){
+  //     console.log(e.target.checked)
+  //     if (e.target.checked){
+  //       //append to array
+  //       this.setState({
+  //         keyGen: this.state.keyGen.concat([value])
+  //       })
+  //     } else {
+  //       //remove from array
+  //       this.setState({
+  //         keyGen : this.state.keyGen.filter(function(val) {return val!==value})
+  //       })
+  //    }
+  // }
 
+  // Checkbox add and remove
+
+  imageChange = (e, pic) => {
+    console.log("pic", pic);
     if (imageDownLoad.includes(pic)) {
-      this.setState({
-        isChecked: !this.state.isChecked
-      });
+      // this.setState({
+      //   isChecked: !this.state.isChecked
+      // });
       let x = imageDownLoad.indexOf(pic);
-      //  alert(x)
+
       imageDownLoad.splice(x, 1);
-    } else {
+    } else if (e.target.checked) {
       imageDownLoad.push(pic);
     }
 
     console.log("dataMatter---", imageDownLoad);
   };
+  // Cancel Button Function
+  cancel = () => {
+    let x = imageDownLoad.lastIndexOf(1);
+    imageDownLoad.splice(x, 1);
+    console.log("dataMatter---", imageDownLoad);
+  };
+
+  // File Downlaod Function
 
   fileDownloadHandler = async (pictures) => {
     console.log("picts", pictures);
@@ -54,28 +81,27 @@ export default class Photo extends React.Component {
     return (
       <div>
         {console.log("dataInside", this.state.imageGallary)}
-        {this.state.imageGallary
-          ? this.state.imageGallary.map((items) => (
-              <div>
-                <img key={items.id} src={items.url} />
-                <input
-                  type="checkbox"
-                  checked={this.state.isChecked}
-                  onChange={() => this.imageChange(items.url)}
-                />
-              </div>
-            ))
-          : ""}
+        {this.state.imageGallary ? (
+          this.state.imageGallary.map((items, i) => (
+            <div>
+              <img key={items} src={items.url} />
+              <input
+                className="checkbox"
+                type="checkbox"
+                onChange={(e) => this.imageChange(e, items.url)}
+              />
+            </div>
+          ))
+        ) : (
+          <p style={{ color: "red", fontSize: "18px" }}>
+            No related Images Found{" "}
+          </p>
+        )}
         <div>
           <button onClick={() => this.fileDownloadHandler(imageDownLoad)}>
-            DownLoad{" "}
+            DownLoad
           </button>
-          <button
-          // // onClick={() => this.fileDownloadHandler(imageDownLoad)}
-          
-          >
-            Cancel
-          </button>
+          <button onClick={() => this.cancel()}>Cancel</button>
         </div>
       </div>
     );
